769b582012e041d38a0e48797f59ded0702fe0f0
Xonoch <xono@andzinski.ovel.pl>
2024-08-04 15:16:02 +0200

    Initial commit

---
+++ src/main/java/net/xono/cellakay/CellaKayClient.java
@@ -0,0 +1,10 @@
+package net.xono.cellakay;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CellaKayClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/xono/cellakay/CellaKayDataGenerator.java
@@ -0,0 +1,11 @@
+package net.xono.cellakay;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CellaKayDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
