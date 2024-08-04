276179ee0e1be2dfbc85302b8884134fa4036caa
Xonoch <xono@andzinski.ovel.pl>
2024-08-04 18:46:24 +0200

    .

--- src/main/java/net/xono/cellakay/block/ModBlocks.java
+++ src/main/java/net/xono/cellakay/block/ModBlocks.java
@@ -4,18 +4,20 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.xono.cellakay.CellaKay;
 
 public class ModBlocks {
     public static final Block RUBY_BLOCK = registerBlock("ruby_block",
             new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK)));
     public static final Block RUBY_ORE_BLOCK = registerBlock("ruby_ore",
-            new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE)));
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.STONE).strength(2f), UniformIntProvider.create(2, 6)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
